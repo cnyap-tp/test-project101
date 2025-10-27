34390cd9c6ea13ca98910485efa846848bdc2541
send2cny <send2cny@gmail.com>
2025-10-27 08:38:54 +0800

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Hello World Script</title></head>
+<body>
+<p> Hello World! in HTML </p>
+
+<?php                                                                        
+	echo "<p>Hello World! in PHP </p>";
+?>                                             
+
+</body>
+</html>
