5130a43c936e055124b59b91660d6e800e193e0f
cnyap-tp <cnyap@tp.edu.sg>
2025-11-03 14:08:32 +0800

    error update

--- index.php
+++ index.php
@@ -1,7 +1,23 @@
-<html>
+<?php
+    $x=5;    // global scope
 
-<body>
-    <p>Hello world</p>
-</body>
+    echo "1st Variable x outside fx()=$x";        // 1st reference to $x
+    echo "<br>";
 
-</html>
+    function fx() {
+        echo "2nd Variable x inside fx()=$x";    // 2nd reference to $x
+        $x=10;
+        echo "3rd Variable x inside fx()=$x";    // 3rd reference to $x
+        echo "<br>";
+    }
+    fx();
+
+    echo "4th Variable x outside fx()=$x";        // 4th reference to $x
+    echo "<br>";
+
+    function gx() {
+        global $x;
+        echo "5th Variable x inside gx()=$x";    // 5th reference to $x
+    }
+    gx();
+?>
