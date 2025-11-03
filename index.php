d0dce50174b0061a0d3f19c99625ff81ab32f12d
cnyap-tp <cnyap@tp.edu.sg>
2025-11-03 13:47:18 +0800

    echo update

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <html>
-<head><title>Hello World Script</title></head>
+
 <body>
-<p> Hello World! in HTML </p>
-
-<?php                                                                        
-	echo "<p>Hello World! in PHP </p>";
-?>                                             
-
+    <p>Hello world</p>
 </body>
+
 </html>
